50ae450021637afbad21952b70d54eacfbd96bc4
Katka Z <219524712+rozrazilrezekvitek@users.noreply.github.com>
2025-07-16 12:42:08 +0200

    castecne funkcni vyhledavani obrazku podle tagu

--- index.php
+++ index.php
@@ -8,15 +8,59 @@
   <link rel="stylesheet" href="style.css">
   <link rel="icon" type="image/png" href="obrazky/gearicon.png">
 </head>
+<?php
+// Get x from previous POST (or set default)
+$tagstring = '';
+// If form submitted with new input, handle it
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['selected_option'])) {
+  $newest_tag = $_POST['selected_option'];
+  $tagstring = $_POST['tagstring'] ?? ''; // x in hidden input  - if nothing from previous input then x=''
+
+  if (preg_match('/^[a-zA-Z0-9 _\-.!?]*$/', $newest_tag)) {
+
+  } else {
+    $newest_tag = '';
+  }
+
+  echo "Received user input: " . htmlspecialchars($newest_tag) . "<br>";
+  echo "Value of tagstring passed along: " . htmlspecialchars($tagstring) . "<br>";
+
+}
+function get_images_for_tag($nazev, $conn): array
+{
+  $sql = "SELECT o.id
+          FROM obrazky o
+          JOIN obrazek_tag ot ON ot.img_id = o.id
+          JOIN tagy t ON t.id = ot.tag_id
+          WHERE t.nazev = '".$nazev."';";
+  echo "------------------------------------> " . $sql;
+  $result = $conn->query($sql);
+  $img_array = [];
+  if ($result->num_rows == 0) {
+    echo "000000000000000000000000000000000000000";
+    return $img_array;
+  } else {
+    while ($row = $result->fetch_assoc()) {
+      $img_array[] = $row["id"];
+      echo "returning............. " . $row["id"];
+    }
+    ;
+    return $img_array;
+  }
+
+}
+
+?>
 
 <body>
   <h1>Meme Machine</h1>
   <div class="central-image-container">
     <img src="obrazky/gearsbulbstill.png">
   </div>
-  <form class="myform" id='nav-form' method='POST' action='sss.php'>
+  <form class="myform" id='nav-form' method='POST' action='index.php'>
     <label for='filter-input'>Search Options:</label>
-    <input type='text' id='filter-input' name="selected_option" placeholder='Type to filter...' aria-controls='filter-select' required autofocus />
+    <input type='text' id='filter-input' name="selected_option" autocomplete="off" placeholder='Type to filter...'
+      aria-controls='filter-select' required autofocus />
     <label for='filter-select'>Choose an option:</label>
     <select id='filter-select' size="6">
       <?php
@@ -24,11 +68,7 @@
       $username = "zmijucha";
       $password = "hnusnypocasipanove";
       $database = "mmm";
-      if (!$_GET || $_GET["id"] == null) {
-        $image_id = 129;
-      } else {
-        $image_id = $_GET["id"];
-      }
+      $max_tags = 4;
       // Create connection
       $conn = new mysqli($servername, $username, $password, $database);
 
@@ -37,12 +77,88 @@
         die("Connection failed: " . $conn->connect_error);
       }
 
-      $sql_all_tags = "SELECT id, nazev FROM tagy";
+      if ($tagstring == '' && $newest_tag == '') {
+        /* žádné zadané pokračuj bez filtrování tagů */
+        echo"/* žádné zadané pokračuj bez filtrování tagů */";
+        $sql_all_tags = "SELECT id, nazev FROM tagy;";
+      }
+      /* existuje pouze jeden (nejnovější) tag */ 
+      elseif ($tagstring == '' && strlen($newest_tag) > 0) {
+        echo"/* existuje pouze jeden (nejnovější) tag */ ";
+        $images_array = get_images_for_tag($newest_tag, $conn);
+        if (count($images_array) == 0) {
+          /* tag je neplatný, pokračuj bez filtrování tagů a bez úpravy tagstringu*/
+          $sql_all_tags = "SELECT id, nazev FROM tagy;";
+        } elseif (count($images_array) == 1) {
+          //konec
+          header("Location: oneimage.php?id=$images_array[0]");
+          exit;
+        } else {
+          $tagstring = "'" . $newest_tag . "'";
+          echo"\nnewest tag p5id8n do tagstringu>". $tagstring ."\n";
+          // filtruj tagy aby tam zůstaly jen takové tagx, že existuje obrázek který má tagx a zároveň $newest_tag
+          echo "// filtruj tagy aby tam zůstaly jen takové tagx, že existuje obrázek který má tagx a zároveň $newest_tag";
+          // pokud takové nejsou --> konec (ale to by byl nějaký eror)
+          $sql_all_tags = "
+                          SELECT nazev FROM tagy t WHERE EXISTS(
+                            SELECT * from obrazek_tag ot WHERE t.id = ot.tag_id AND EXISTS(
+                              SELECT * from obrazek_tag ot2 WHERE 
+                                ot2.img_id = ot.img_id AND ot2.tag_id IN (
+                                  SELECT id FROM tagy tt WHERE  tt.nazev = '".$newest_tag."'))) AND t.nazev != '".$newest_tag."';";
+        }
+      }
+      elseif (count(explode(",", $tagstring)) >= $max_tags) {
+        echo "/* newest_tag je neprázdný a tagstring taky, tagstring >max */";
+        /* newest_tag je neprázdný a tagstring taky, tagstring >max */
+        //konec, vyber na základě předchozích
+        header("Location: oneimage.php?id=$img");//xxx dodelat
+        exit;
+      }
+      else {
+        /* newest_tag je neprázdný a tagstring taky, tagstring <max */ 
+        echo"/* newest_tag je neprázdný a tagstring taky, tagstring <max */ ";
+        $images_array = get_images_for_tag($newest_tag, $conn);
+        if (count($images_array) == 0) {
+          /* poslední tag je neplatný, --> konec (vyber na základě předchozích) xxx dodelat!!!!!*/
+          echo"/* poslední tag je neplatný, --> konec (vyber obrazek na základě předchozích -> header, exit) xxx dodelat!!!!!*/";
+          //$sql_all_tags = "SELECT id, nazev FROM tagy";
+          header("Location: oneimage.php?id=$images_array[0]");
+          exit;
+        } elseif (count($images_array) == 1) {
+          //konec
+          header("Location: oneimage.php?id=$images_array[0]");
+          exit;
+        } else {
+          $tagstring = $tagstring . ",'" . $newest_tag . "'";
+          // filtruj tagy aby tam zůstaly jen takové tagx, že existuje obrázek který má tagx a zároveň všechny z tagstringu s přidaným newest
+          // xxx pozor aby tam nezustal newest !!!!!!!!!!!!!!
+          // pokud takové nejsou --> konec
+          $sql_all_tags = "
+                SELECT name FROM tagy t WHERE
+                    EXISTS(
+                        SELECT * FROM obrazky o WHERE
+                            EXISTS(
+                                SELECT * FROM obrazek_tag ot WHERE
+                                    to.tag_id = t.id AND to.img_id = o.id AND
+                                    NOT EXISTS(
+                                        SELECT * FROM tagy tt WHERE
+                                        tt IN (" . $tagstring . ") AND
+                                        NOT EXISTS SELECT * FROM obrazek_tag oott WHERE
+                                        oott.tag_id = tt.id AND oott.img_id = o.id
+                                    )
+                            )
+                    );";
+
+        }
+      }
+
+
+      echo"\nsql_all_tags ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n$sql_all_tags";
       $tagy = $conn->query($sql_all_tags);
       while ($row = $tagy->fetch_assoc()) {
         $tag_id = $row["id"];
         $tag_nazev = $row["nazev"];
-        echo "<option value='" . $tag_id . "'>" . $tag_nazev . "</option>";
+        echo "<option value='" . $tag_id . "'>" . $tag_nazev . "</option>"; //tady se ty tagy nasypou do UI selectu
       }
       echo "</select>
         <button type='submit'>Boom</button> 
@@ -51,9 +167,10 @@
       ?>
       <script>
         const input = document.getElementById("filter-input");
+        const hidden_input = document.getElementById("tagstring");
         const select = document.getElementById("filter-select");
         const form = document.getElementById("nav-form");
-
+ 
         let isBackspacing = false;
         let isTyping = false;
 
@@ -120,6 +237,8 @@
           if (key === "Enter") {
             e.preventDefault();
             form.requestSubmit();
+            hidden_input.value=$tagstring;
+            
             return;
           }
 
@@ -162,6 +281,8 @@
       </script>
 
 
+      <!-- hidden input to pass array along -->
+      <input type="hidden" name="tagstring" value="<?php echo htmlspecialchars($tagstring) ?>">
 
 </body>
 
